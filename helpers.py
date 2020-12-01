7e09212c86d2b0ec0c6a633e7a651869c3a93a0d
Zaheer Abbas <nk4456542@gmail.com>
2020-12-01 22:10:16 +0530

    Add a comment to explain the size of the Buffer

--- helpers.py
+++ helpers.py
@@ -1,10 +1,15 @@
 import socket
+
 LOCAL_HOST = "localhost"
 LOCAL_DNS_SERVER_PORT = 9000
 ROOT_SERVER_PORT = 9001
 TLD_SERVER_PORT = 9002
 AUTHORITATIVE_SERVER_PORT = 9003
 BUFFER_SIZE = 65535
+# Note:
+# We could add the buffer size variable, one for sending and one for receiving,
+# I have only used one to keep it simple. And its size is based on the max size
+# of the receiving buffer
 
 
 def customPrint(name, value):
